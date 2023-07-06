ec9cab561a0a17d2fae72325f67b569047c35d44
Hardik Chouhan <hardikchouhan1901@gmail.com>
2023-07-06 14:01:22 +0530

    error handling & fetch issue resolved

--- random_jokes_api/random_jokes.js
+++ random_jokes_api/random_jokes.js
@@ -1,28 +1,23 @@
 const JokeApiUrl = "https://v2.jokeapi.dev/joke/Any?type=single&amount=2"
 
 let show = document.getElementById('joke_here')
-
-let random = Math.floor(Math.random()*10);
-console.log(random);
-
 let get_joke
 
-get_new_joke = () => {
-
-    async function cloud_fetch() {
-        const response = await fetch(JokeApiUrl)
-        
-        let data = await response.json()
-        console.log(data)
-        console.log(data.jokes)
-        get_joke  = data.jokes[0]
-    }
-    
-    cloud_fetch()
-    // console.log(get_joke.joke);
-    setTimeout(() => {
+async function cloud_fetch() {
+    const response = await fetch(JokeApiUrl)   
+    let data = await response.json()
+    console.log(data)
+    console.log(data.jokes)
+    get_joke  = data.jokes[0]
+}
+get_new_joke = async () => {
+    try {
+        await cloud_fetch()
         console.log(get_joke.joke);
         show.innerHTML = `${get_joke.joke}`
-    }, 100);
-    
+    } 
+    catch (e) {
+        console.error('Error Message : '+e);
+        show.innerHTML = `<b>${e}<b>`
+    }
 }
